001c97578e9f9e4705e7f93b4173053639e3fda7
Atsuto Tatsumi <atsutout@gmail.com>
2024-03-31 22:18:13 +0900

    Able to run locally

--- app.py
+++ app.py
@@ -8,87 +8,65 @@ import os
 import queue
 import datetime
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer
+from streamlit_webrtc import webrtc_streamer, WebRtcMode
 import plotly.express as px
+import av
 
-def capture_camera():
+@st.cache_resource
+def load_model():
     #Load fine-tuned YOLO detection model
     model = torch.hub.load('ultralytics/yolov5', 'custom', path='yolov5/runs/train/exp5/weights/last.pt', force_reload=True)
+    return model
 
-    log = []
-    #Open webcam to capture images
-    cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
-    while cap.isOpened():
-        ret, frame = cap.read()
-        # Make detections using the model
-        results = model(frame)
+cap = cv2.VideoCapture(0)
+model = load_model()
+log = []
 
-        #Extracting detected labels and detected time
-        predictions = results.pred[0]
-        detected_labels = predictions[:,-1].cpu().numpy()
-        current_time = datetime.datetime.now()
-        hour_minute = current_time.strftime("%H:%M:%S")
-        #Taking logs of events
-        class_names = results.names
-        detected_class_names = [class_names[label] for label in detected_labels]
-        if detected_class_names != []:
-            for word in detected_class_names:
-                log.append([word,hour_minute])
+#Basic structure of the webpage
+st.title("Archangel")
+frame_placeholder = st.empty()
 
-        cv2.imshow('YOLO', np.squeeze(results.render()))
+#Add "Stop" button and store its state in a variable
+stop_button_pressed = st.button("Stop")
 
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            break
-    cap.release()
-    cv2.destroyAllWindows()
+#Livestream webcam using OpenCV
+while cap.isOpened() and not stop_button_pressed:
+    ret, frame = cap.read()
+    if not ret:
+        st.write("The video capture has ended.")
+        break
+    #Applying finetuned YOLO model to webcam
+    results = model(frame)
 
-    #Show the log of your pet.
-    #combined_data = np.array([[event, time] for event, times in log.items() for time in times])
-    log = np.array(log)
-    return log
+    #Extracting detected labels and detected time
+    predictions = results.pred[0]
+    detected_labels = predictions[:,-1].cpu().numpy() #list of numbers
+    current_time = datetime.datetime.now()
+    hour_minute = current_time.strftime("%H:%M:%S")
+    #Taking logs of events
+    class_names = results.names #dictionary
+    detected_class_names = [class_names[label] for label in detected_labels]
+    if detected_class_names != []:
+        for word in detected_class_names:
+            log.append([word,hour_minute])
+            #Storing log as session state
+            if 'log' not in st.session_state:
+                st.session_state.log = log
+    #Displaying the webcam on the webpage
+    results_array = np.squeeze(results.render())
+    stream = cv2.cvtColor(results_array,cv2.COLOR_BGR2RGB)
+    frame_placeholder.image(stream,channels="RGB")
 
-def show_figure(log):
-    fig = px.scatter(x=log[:,1],y=log[:,0])
-    fig.show()
+    # Break the loop if the 'q' key is pressed or the user clicks the "Stop" button
+    if cv2.waitKey(1) & 0xFF == ord("q") or stop_button_pressed:
+        break
 
-#webrtc_streamer(key='sample')
-# col1, col2 = st.columns([2.8,0.3,3])
-# col1.write(" ")
-# with col1:
-#     st.subheader("Video Input")
-#     #Camera Image
-#     with st.form("image_input"):
-#         captured_image =
-result_queue: "queue.Queue[List[Detection]]" = queue.Queue() #type:ignore
-second_queue: "queue.Queue[List[Detection]]" = queue.Queue() #type:ignore
+cap.release()
+cv2.destroyAllWindows()
 
-camera_load_state = st.text('Preparing camera...')
-ctx = webrtc_streamer(key='example',
-                      video_frame_callback=capture_camera,
-                      rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
-                      media_stream_constraints={"video": True, "audio": False},
-                      async_processing=True)
+#Showing the log as a graph after the webcam is closed
+log_array = np.array(st.session_state.log)
+fig = px.scatter(x=log_array[:,1],y=log_array[:,0])
+fig.show()
 
-if ctx.state.playing:
-        frame_count = 0
-        frame_counter_placeholder = st.empty()
-        frame_counter_placeholder.text("Recording... 0%")
-
-        result = ""
-        prediction_placeholder = st.empty()
-
-        while True:
-            frame_count = (second_queue.get() + 1) * 100 / 20
-            frame_counter_placeholder.text(f"📽️ Recording... {frame_count: .0f}%")
-
-            if frame_count == 100:
-                frame_counter_placeholder.text("🛠️ AI at work... 🦾")
-                result += result_queue.get() + " -> "
-                prediction_placeholder.markdown(f"<h1>{result}</h1>", unsafe_allow_html=True)
-
-
-
-# if st.checkbox('Show the log'):
-#     st.subheader('Here is the log of today.')
-#     figure = show_figure(camera_output)
-#     st.write(figure)
+#if __name__ = '__main__':
