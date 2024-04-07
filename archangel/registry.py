b980d99a3dd91070f13d19151a3bf12f7054ce62
Atsuto Tatsumi <atsutout@gmail.com>
2024-04-07 14:24:49 +0900

    Configuring GCP environment

---
+++ archangel/registry.py
@@ -0,0 +1,16 @@
+from google.cloud import storage
+
+
+def save_model():
+    if MODEL_TARGET == "gcs":
+        # 🎁 We give you this piece of code as a gift. Please read it carefully! Add a breakpoint if needed!
+
+        model_filename = model_path.split("/")[-1] # e.g. "20230208-161047.h5" for instance
+        client = storage.Client()
+        bucket = client.bucket(BUCKET_NAME)
+        blob = bucket.blob(f"models/{model_filename}")
+        blob.upload_from_filename(model_path)
+
+        print("✅ Model saved to GCS")
+
+    return None
